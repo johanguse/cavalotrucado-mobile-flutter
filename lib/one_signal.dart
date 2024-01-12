de72f3b3c64bddb42a729c4a71814d4735442534
Johan Guse <johanguse@gmail.com>
2024-01-12 02:48:55 -0300

    feat: update dependencies

--- lib/one_signal.dart
+++ lib/one_signal.dart
@@ -1,20 +1,28 @@
 import 'dart:developer';
 import 'package:onesignal_flutter/onesignal_flutter.dart';
 
-
 class AppOneSignalController {
-  Future initPlatformState() async{
-    try{
-      await OneSignal.shared.setAppId("cb8e4e73-677c-4c6a-a68b-ded7efca1577");
-      OneSignal.shared
-          .promptUserForPushNotificationPermission(fallbackToSettings: false)
-          .then((accepted) async {
-        log('Cliente aceitou permissão: $accepted');
+  Future initPlatformState() async {
+    try {
+      // Initialize OneSignal
+      OneSignal.initialize("cb8e4e73-677c-4c6a-a68b-ded7efca1577");
+
+      // Clear all notifications (optional)
+      OneSignal.Notifications.clearAll();
+
+      // Setting Listeners
+      OneSignal.Notifications.addPermissionObserver((state) {
+        log('Cliente aceitou permissão: ${state.toString()}');
       });
-    }
-    catch(e,s){
-      log(e as String);
-      log(s as String);
+
+      // Request permission for push notifications
+      OneSignal.Notifications.requestPermission(true);
+
+      // Add more listeners and handlers as per your requirements
+
+    } catch (e, s) {
+      log(e.toString());
+      log(s.toString());
       log("Erro ao configurar OneSignal");
     }
   }
